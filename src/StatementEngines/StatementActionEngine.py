89111cdd04cbd41ee62176e7c144842e83837e18
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2024-05-24 11:51:23 +1200

    skeleton code for statement engines

---
+++ src/StatementEngines/StatementActionEngine.py
@@ -0,0 +1,13 @@
+from src.agent.Agent import Agent
+from src.game.BettingGame import BettingGame
+from src.game.BettingRound import BettingRound
+
+
+class StatementActionEngine:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def evaluate(agent : Agent, game : BettingGame, round : BettingRound) -> bool:
+        pass # TODO
