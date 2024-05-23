c2caa36719a0cd71702ef84af3ee6fd5ccd9952f
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2024-05-24 11:51:31 +1200

    skeleton code for engines

---
+++ src/StatementEngines/StatementEmotionEngine.py
@@ -0,0 +1,18 @@
+from src.agent.EmotionChange import EmotionChange
+from src.agent.Agent import Agent
+from src.game.BettingGame import BettingGame
+from src.game.BettingRound import BettingRound
+
+class StatementEmotionEngine:
+
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def evaluate(agent : Agent, game: BettingGame, round : BettingRound) -> EmotionChange:
+        # Parameters can be static - set when initialising the game
+        pass # TODO
+
+
+    
+
