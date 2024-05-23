2fb65212195532ef343ba01c5c6595736bcabc39
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2024-05-24 09:21:11 +1200

    little fix to play_round

--- src/Player.py
+++ src/Player.py
@@ -9,12 +9,12 @@ class Player:
         # TODO: Implement this
         pass
 
-    def play_round(self, betting_round: BettingRound, past_rounds: list) -> bool:
+    def play_round(self, betting_round: BettingRound, past_rounds: list[BettingRound]) -> bool:
         """
         Plays a round of betting
 
         :param betting_round: round to play
-        :param past_rounds: past rounds played. To create some idea of 'luck' for the player
+        :param past_rounds: past rounds played in a game. To create some idea of 'luck' for the player
         :return: boolean whether the player will take the bet or not
         """
         # Returns a boolean whether the player will take the bet or not
