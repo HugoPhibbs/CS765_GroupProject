cbea2f3751a50f0fadbd7daea04505f17c039443
Hugo Phibbs <hugophibbs1@gmail.com>
2024-05-30 11:36:02 +1200

    comments to main.py

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from src.agent.Agent import Agent
 from src.agent.AgentType import AgentType
 from src.game.BettingGame import BettingGame
 
+# Creating an agent
 agent = Agent(agent_type = AgentType.RATIONAL) 
-
 # agent = Agent(AgentType.CONSERVATIVE)
 
+# Creating a game instance
 game = BettingGame(
     num_rounds = 10, 
     coins_per_round = 10,
     starting_cash =  100
     )
 
+# Playing the game
 game.play_game(
     agent=agent, 
     skip_time_outs=False
